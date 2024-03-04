5d262a7188994bbbc34f2443928c0e995ac2d35f
GustavoWustemberg <gustavowustemberg14@gmail.com>
2024-03-04 19:37:47 -0300

    Alter database

--- BackEnd/LoginApi/DataBank/dbConnection.py
+++ BackEnd/LoginApi/DataBank/dbConnection.py
@@ -1,3 +1,3 @@
 from peewee import MySQLDatabase
 
-db = MySQLDatabase('userDb', user='root', password='', host='localhost')
+db = MySQLDatabase('bicywj5s4qmlllukzwdx', user='uw7ppaigfjciv2hi', password='qVOhT282XpXWbiX2ATi9', host='bicywj5s4qmlllukzwdx-mysql.services.clever-cloud.com')
